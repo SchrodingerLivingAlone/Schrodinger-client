f40e1ac023538d9835ca86ced5a9904786fa95d7
rbgksqkr <rbgks1937@naver.com>
2023-11-10 02:20:22 +0900

    feat: 동네정보화면 TabBarView 생성

---
+++ lib/style.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
+
+class AppColor {
+  // static const backgroundColor = Color(0xff2f4550);
+
+  static const main = Color(0xff4600A3);
+}
