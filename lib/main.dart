34b9b9b58a5ce8ad18f96f9b7ac6221ae544a445
rbgksqkr <rbgks1937@naver.com>
2023-11-10 02:36:57 +0900

    feat: ListView 생성

--- lib/main.dart
+++ lib/main.dart
@@ -55,35 +55,115 @@ class MyHomePage extends StatelessWidget {
           ),
           body: TabBarView(
             children: [
-              Tab(
-                child: Align(
-                  alignment: Alignment.bottomRight,
-                  child: Container(
-                    color: Colors.red,
-                    width: 100,
-                    height: 100,
+              ListView(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(top:10, bottom: 10),
+                    child: ListTile(
+                      title: const Text('기대치에 못미쳤던.. 백미당 후기'),
+                      subtitle: const Text('유명한 백미당에 가봤는데 후기에서 봤던거랑 다르게...'),
+                      trailing: const Icon(Icons.square, size: 50),
+                      onTap: (){},
+                    ),
                   ),
+                  Padding(
+                    padding: const EdgeInsets.only(top:10, bottom: 10),
+                    child: ListTile(
+                      title: const Text('기대치에 못미쳤던.. 백미당 후기'),
+                      subtitle: const Text('유명한 백미당에 가봤는데 후기에서 봤던거랑 다르게...'),
+                      trailing: const Icon(Icons.square, size: 50),
+                      onTap: (){},
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top:10, bottom: 10),
+                    child: ListTile(
+                      title: const Text('기대치에 못미쳤던.. 백미당 후기'),
+                      subtitle: const Text('유명한 백미당에 가봤는데 후기에서 봤던거랑 다르게...'),
+                      trailing: const Icon(Icons.square, size: 50),
+                      onTap: (){},
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top:10, bottom: 10),
+                    child: ListTile(
+                      title: const Text('기대치에 못미쳤던.. 백미당 후기'),
+                      subtitle: const Text('유명한 백미당에 가봤는데 후기에서 봤던거랑 다르게...'),
+                      trailing: const Icon(Icons.square, size: 50),
+                      onTap: (){},
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top:10, bottom: 10),
+                    child: ListTile(
+                      title: const Text('기대치에 못미쳤던.. 백미당 후기'),
+                      subtitle: const Text('유명한 백미당에 가봤는데 후기에서 봤던거랑 다르게...'),
+                      trailing: const Icon(Icons.square, size: 50),
+                      onTap: (){},
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top:10, bottom: 10),
+                    child: ListTile(
+                      title: const Text('기대치에 못미쳤던.. 백미당 후기'),
+                      subtitle: const Text('유명한 백미당에 가봤는데 후기에서 봤던거랑 다르게...'),
+                      trailing: const Icon(Icons.square, size: 50),
+                      onTap: (){},
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top:10, bottom: 10),
+                    child: ListTile(
+                      title: const Text('기대치에 못미쳤던.. 백미당 후기'),
+                      subtitle: const Text('유명한 백미당에 가봤는데 후기에서 봤던거랑 다르게...'),
+                      trailing: const Icon(Icons.square, size: 50),
+                      onTap: (){},
+                    ),
+                  ),
+
+
+                ],
+              ),
+
+                ListTile(
+                  leading: const Icon(Icons.search),
+                  title: const Text('Search'),
+                  trailing: const Icon(Icons.navigate_next),
+                  onTap: (){},
                 ),
-              ),
-              Container(
-                color: Colors.green,
-              ),
-              Container(
-                color: Colors.blue,
-              ),
-              Container(
-                color: Colors.red,
-              ),
-              Container(
-                color: Colors.blue,
-              ),
-              Container(
-                color: Colors.green,
-              ),
-              Container(
-                color: Colors.red,
-              ),
-            ],
+                ListTile(
+                  leading: const Icon(Icons.refresh),
+                  title: const Text('Refresh'),
+                  trailing: const Icon(Icons.navigate_next),
+                  onTap: (){},
+                ),
+                ListTile(
+                  leading: const Icon(Icons.home),
+                  title: const Text('Home'),
+                  trailing: const Icon(Icons.navigate_next),
+                  onTap: (){},
+                ),
+                ListTile(
+                  leading: const Icon(Icons.search),
+                  title: const Text('Search'),
+                  trailing: const Icon(Icons.navigate_next),
+                  onTap: (){},
+                ),
+                ListTile(
+                  leading: const Icon(Icons.refresh),
+                  title: const Text('Refresh'),
+                  trailing: const Icon(Icons.navigate_next),
+                  onTap: (){},
+                ),
+                ListTile(
+                  leading: const Icon(Icons.refresh),
+                  title: const Text('ㅁㄴㅇㅁㄴㅇㅁㄴ'),
+                  trailing: const Icon(Icons.navigate_next),
+                  onTap: (){},
+                ),
+              ],
+
+
           ),
           drawer: const Drawer(),
           bottomNavigationBar: BottomNavigationBar(
