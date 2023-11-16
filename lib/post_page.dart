a2f0a50d5f85767ba9e0a7ae8b1fe6d5e403cd1e
JunHyeok Lee <ljh102715@naver.com>
2023-11-16 09:11:47 +0900

    feat: 기본 ui 완성

--- lib/post_page.dart
+++ lib/post_page.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:schrodinger_client/style.dart';
 
-//1.주제 누르면 하나만 선택되게 (색바꾸기)
-//2.주제, 내용 입력받기
-//3.이미지 GridView 갯수만큼만 띄우기
-//4.장소
+//1.등록하면 글 객체 생성
+//2.이미지 GridView 갯수만큼만 띄우기
+//3.장소
+
+enum Issue {Restaurant, Facility, Discount, Etc, Together, Ask, PublicInfo}
 
 class PostPage extends StatefulWidget {
   const PostPage({super.key});
@@ -15,15 +16,35 @@ class PostPage extends StatefulWidget {
 
 class _PostPageState extends State<PostPage> {
 
+  Issue _issue = Issue.Restaurant;
+  String selectedButton = 'Button 1';
 
+  final _titleController = TextEditingController();
+  final _contentController = TextEditingController();
+
+  void selectButton(String buttonName) {
+    setState(() {
+      selectedButton = buttonName;
+    });
+  }
+  Color getButtonColor(String buttonName) {
+    return selectedButton == buttonName ? Colors.orange : Colors.grey;
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        iconTheme: const IconThemeData(
+            iconTheme: const IconThemeData(
             color: Colors.black
         ),
+        leading:  IconButton(
+          onPressed: () {
+            showExitConfirmationDialog(context);
+
+          },
+          color: Colors.purple,
+          icon: Icon(Icons.arrow_back)),
         backgroundColor: Colors.white,
         title: const Center(
             child: Text('새 게시물', style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold))),
@@ -63,32 +84,49 @@ class _PostPageState extends State<PostPage> {
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
-                ElevatedButton(onPressed: (){},
+                ElevatedButton(onPressed: (){
+                    selectButton('Button 1');
+                    _issue = Issue.Restaurant;
+                  },
                     style: ElevatedButton.styleFrom(
                         shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
                         minimumSize: Size(30, 30),
-                        primary: Colors.grey, elevation: 10),
+                        primary: getButtonColor('Button 1'),
+                        elevation: 10
+                    ),
                     child: Text('맛집', style: TextStyle(fontSize: 13),)
                 ),
-                ElevatedButton(onPressed: (){},
+                ElevatedButton(onPressed: (){
+                  selectButton('Button 2');
+                  _issue = Issue.Facility;
+                },
                     style: ElevatedButton.styleFrom(
                         shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
                         minimumSize: Size(30, 30),
-                        primary: Colors.grey, elevation: 10),
+                        primary: getButtonColor('Button 2'),
+                        elevation: 10),
                     child: Text('시설', style: TextStyle(fontSize: 13),)
                 ),
-                ElevatedButton(onPressed: (){},
+                ElevatedButton(onPressed: (){
+                  selectButton('Button 3');
+                  _issue = Issue.Discount;
+                },
                     style: ElevatedButton.styleFrom(
                         shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
                         minimumSize: Size(30, 30),
-                        primary: Colors.grey, elevation: 10),
+                        primary: getButtonColor('Button 3'),
+                        elevation: 10),
                     child: Text('할인', style: TextStyle(fontSize: 13),)
                 ),
-                ElevatedButton(onPressed: (){},
+                ElevatedButton(onPressed: (){
+                  selectButton('Button 4');
+                  _issue = Issue.Etc;
+                  },
                     style: ElevatedButton.styleFrom(
                         shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
                         minimumSize: Size(30, 30),
-                        primary: Colors.grey, elevation: 10),
+                        primary: getButtonColor('Button 4'),
+                        elevation: 10),
                     child: Text('기타', style: TextStyle(fontSize: 13),)
                 ),
               ],
@@ -96,25 +134,37 @@ class _PostPageState extends State<PostPage> {
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
-                ElevatedButton(onPressed: (){},
+                ElevatedButton(onPressed: (){
+                  selectButton('Button 5');
+                  _issue = Issue.Together;
+                },
                     style: ElevatedButton.styleFrom(
                         shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
                         minimumSize: Size(30, 30),
-                        primary: Colors.grey, elevation: 10),
+                        primary: getButtonColor('Button 5'),
+                        elevation: 10),
                     child: Text('같이해요', style: TextStyle(fontSize: 13),)
                 ),
-                ElevatedButton(onPressed: (){},
+                ElevatedButton(onPressed: (){
+                  selectButton('Button 6');
+                  _issue = Issue.Ask;
+                },
                     style: ElevatedButton.styleFrom(
                         shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
                         minimumSize: Size(30, 30),
-                        primary: Colors.grey, elevation: 10),
+                        primary: getButtonColor('Button 6'),
+                        elevation: 10),
                     child: Text('질문/요청', style: TextStyle(fontSize: 13),)
                 ),
-                ElevatedButton(onPressed: (){},
+                ElevatedButton(onPressed: (){
+                  selectButton('Button 7');
+                  _issue = Issue.PublicInfo;
+                },
                     style: ElevatedButton.styleFrom(
                         shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
                         minimumSize: Size(30, 30),
-                        primary: Colors.grey, elevation: 10),
+                        primary: getButtonColor('Button 7'),
+                        elevation: 10),
                     child: Text('공공정보', style: TextStyle(fontSize: 13),)
                 ),
               ],
@@ -144,9 +194,17 @@ class _PostPageState extends State<PostPage> {
               ),
             ),
             Padding(
-              padding: const EdgeInsets.fromLTRB(30, 10, 0, 5),
+              padding: const EdgeInsets.fromLTRB(30, 10, 30, 5),
               child: Container(
-                child: Text('제목', style: TextStyle(fontSize: 15,color: Colors.grey, fontWeight: FontWeight.bold)),
+                child: TextField(
+                  maxLines: null,
+                  maxLength: 30,
+                  decoration: InputDecoration(
+                    border: InputBorder.none,
+                    hintText: '제목을 입력하세요.'
+                  ),
+                  controller: _titleController,
+                ),
               ),
             ),
             Padding(
@@ -156,9 +214,17 @@ class _PostPageState extends State<PostPage> {
                 color:Colors.black,),
             ),
             Padding(
-              padding: const EdgeInsets.fromLTRB(30, 10, 0, 5),
+              padding: const EdgeInsets.fromLTRB(30, 10, 30, 5),
               child: Container(
-                child: Text('내용을 입력하세요', style: TextStyle(fontSize: 15,color: Colors.grey, fontWeight: FontWeight.bold)),
+                child: TextField(
+                  maxLines: null,
+                  maxLength: 200,
+                  decoration: InputDecoration(
+                    border: InputBorder.none,
+                    hintText: '내용을 입력하세요',
+                  ),
+                  controller: _contentController,
+                ),
               ),
             ),
           ],//children
@@ -166,4 +232,23 @@ class _PostPageState extends State<PostPage> {
       ),
     );
   }
+}
+
+Future<bool?> showExitConfirmationDialog(BuildContext context) async {
+  return showDialog<bool?>(
+    context: context,
+    builder: (context) => AlertDialog(
+      content: Text('작성중인 글을 취소하시겠습니까?\n작성 취소 시, 작성된 글은 저장되지 않습니다.', style: TextStyle(fontSize: 15),),
+      actions: <Widget>[
+        TextButton(
+          onPressed: () => Navigator.of(context).pop(false),
+          child: Text('계속 작성', style: TextStyle(color: Colors.blue),),
+        ),
+        TextButton(
+          onPressed: () => Navigator.of(context, rootNavigator: true).pop(true),
+          child: Text('작성 취소', style: TextStyle(color: Colors.red),),
+        ),
+      ],
+    ),
+  );
 }
