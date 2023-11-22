3a940bea2826ace069ffccc576b09ba487c2b07a
rbgksqkr <rbgks1937@naver.com>
2023-11-22 22:14:50 +0900

    feat: 동네 인증화면 하단부 퍼블리싱(#3)

--- lib/town_info/town_auth.dart
+++ lib/town_info/town_auth.dart
@@ -18,6 +18,8 @@ class _TownAuthPageState extends State<TownAuthPage> {
         ),
         leading:  IconButton(
             onPressed: () {
+              Navigator.pop(context);
+              Navigator.pop(context);
             },
             color: Colors.purple,
             icon: const Icon(Icons.arrow_back)),
@@ -34,44 +36,62 @@ class _TownAuthPageState extends State<TownAuthPage> {
               child: const Icon(Icons.help_outline_rounded, color: Colors.black)),
         ],
       ),
-      body: Row(
+      body: Column(
         children: [
-          Expanded(
-            child: Stack(
+            Stack(
               alignment: Alignment.bottomRight,
-          children: [
-            Container(
-              height: 300,
-              color: Colors.grey,
-            ),
-            Padding(
-              padding: EdgeInsets.all(10),
-              child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                      backgroundColor: AppColor.yellow,
-                      shape: const CircleBorder(),
-                      padding: const EdgeInsets.all(5)
-                  ),
-                  onPressed: (){},
-                  child: const SizedBox(
-                    width: 60,
-                    height: 60,
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Icon(Icons.place),
-                        Text('현재위치', style: TextStyle(fontSize: 12)),
-                      ],
-                    ),
+              children: [
+                Container(
+                  height: 350,
+                  color: AppColor.lightGrey
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(10),
+                  child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                          backgroundColor: AppColor.yellow,
+                          shape: const CircleBorder(),
+                          padding: const EdgeInsets.all(5)
+                      ),
+                      onPressed: (){},
+                      child: const SizedBox(
+                        width: 60,
+                        height: 60,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Icon(Icons.place),
+                            Text('현재위치', style: TextStyle(fontSize: 12)),
+                          ],
+                        ),
+                      )
                   )
+                ),
+              ],
+            ),
+          Column(
+            children: [
+              Padding(
+                padding: EdgeInsets.all(20),
+                child: Container(
+                  // width: 200,
+                  height: 120,
+                  // color: Colors.red,
+                  child: const Text("현재 위치는 ‘ **동’ 입니다.\n\n거주하시는 동네가 맞다면 아래 버튼을 눌러\n동네 인증을 완료해주세요."),
+                ),
+              ),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: AppColor.main,
+                  shape: const StadiumBorder(),
+                  padding: const EdgeInsets.only(top: 16, bottom: 16, left: 90, right: 90)
+                ),
+                  onPressed: (){},
+                  child: const Text('동네인증 완료하기', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),)
               )
-            )
-
-        ],
-      )
-
-
+            ],
           )
+
         ],
       )
     );
