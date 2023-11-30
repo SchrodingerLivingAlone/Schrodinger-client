aad4aca3663ce483b24768bd0d9e30f79b8db42c
JunHyeok Lee <ljh102715@naver.com>
2023-11-30 10:34:50 +0900

    feat: Post ui implementation완료

--- lib/post/post_info.dart
+++ lib/post/post_info.dart
@@ -1,3 +1,5 @@
+import 'package:carousel_slider/carousel_controller.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:schrodinger_client/post/post_adjust.dart';
 
@@ -14,9 +16,76 @@ class PostInfo extends StatefulWidget {
 }
 
 class _PostInfoState extends State<PostInfo> {
+  int current = 0;
+  final CarouselController _carouselController = CarouselController();
+  List imageList = [
+    "https://capstoneroomof.s3.ap-northeast-2.amazonaws.com/Image/lhs3.jpgb449133c-4efa-41de-b9d0-bf15dff2c805",
+    "https://capstoneroomof.s3.ap-northeast-2.amazonaws.com/Image/lhs3.jpgb449133c-4efa-41de-b9d0-bf15dff2c805",
+    "https://schrodinger-cau.s3.ap-northeast-2.amazonaws.com/2509b321-65ce-4fa9-9112-06af0e367e5d.png"
+  ];
   bool isScrapped = false;
   bool isLiked = false;
   final _commentController = TextEditingController();
+
+  Widget sliderWidget () {
+    return CarouselSlider (
+        carouselController: _carouselController,
+        items: imageList.map(
+          (imgLink) {
+            return Builder(
+              builder: (context) {
+                return SizedBox(
+                  width: MediaQuery
+                      .of(context)
+                      .size
+                      .width,
+                  child: Image(
+                    fit: BoxFit.contain,
+                    image: NetworkImage(
+                      imgLink,
+                    ),
+                  ),
+                );
+          },
+      ); // Builder
+    },
+  ).toList (),
+  options: CarouselOptions (
+      height: 300,
+      viewportFraction: 1.0,
+      autoPlay: false,
+      autoPlayInterval: const Duration (seconds: 4),
+      onPageChanged: (index, reason) {
+          setState(() {
+          current = index;
+          });
+          },
+      ),
+    );
+  }//
+
+  Widget sliderIndicator() {
+    return Align(
+        alignment: Alignment. bottomCenter,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment. center,
+          children: imageList.asMap().entries.map((entry){
+            return GestureDetector(
+              onTap: () => _carouselController.animateToPage(entry.key),
+                  child: Container(
+                    width: 12,
+                    height: 12,
+                    margin:
+                        const EdgeInsets.symmetric(vertical: 8.0, horizontal: 4.0),
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: Colors.white.withOpacity(current == entry.key ? 0.9 : 0.4),),
+                  ),
+            );// GestureDetector
+          }).toList(),
+        ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -126,23 +195,8 @@ class _PostInfoState extends State<PostInfo> {
                     child: Stack(
                         alignment: Alignment.bottomRight,
                         children: [
-                          Container(
-                            width: 400.0,
-                            height: 200.0,
-                            color: Colors.grey,
-                            child: const Text('Image Position'),
-                            // Add any child widgets here if needed.
-                          ),
-                          const Padding(
-                            padding: EdgeInsets.all(10.0),
-                            child: Row(
-                                mainAxisAlignment: MainAxisAlignment.end,
-                                children: [
-                                  Icon(Icons.pin_drop_outlined, color: Colors.deepPurple,),
-                                  Text('위치', style: TextStyle(color: Colors.deepPurple),),
-                              ],
-                            ),
-                          )
+                          sliderWidget(),
+                          sliderIndicator(),
                         ],
                       ),
                   ),
@@ -151,37 +205,53 @@ class _PostInfoState extends State<PostInfo> {
                     child: Column(
                           children: [
                             Row(
-                              mainAxisAlignment: MainAxisAlignment.end,
                               children: [
-                                ElevatedButton(
-                                    style: ElevatedButton.styleFrom(
-                                        shape: RoundedRectangleBorder(
-                                          borderRadius: BorderRadius.circular(10.0), // 조절하고 싶은 둥근 정도
-                                        ),
-                                      backgroundColor: isLiked ? Colors.blue : Colors.white,
-                                      shadowColor: Colors.transparent,
+                                Expanded(
+                                  child: Padding(
+                                    padding: const EdgeInsets.fromLTRB(8.0, 0, 0, 0),
+                                    child: Row(
+                                      mainAxisAlignment: MainAxisAlignment.start,
+                                      children: [
+                                        Icon(Icons.pin_drop_outlined, color: Colors.deepPurple, size: 20,),
+                                        Text('위치', style: TextStyle(color: Colors.deepPurple, fontSize: 15),),
+                                      ],
                                     ),
-                                    onPressed: (){
-                                      setState(() {
-                                        isLiked = !isLiked;
-                                      });
-                                    },
-                                    child: const Text('공감하기', style: TextStyle(fontSize: 15, color:Colors.black),)
+                                  ),
                                 ),
-                                ElevatedButton(
-                                    style: ElevatedButton.styleFrom(
-                                      shape: RoundedRectangleBorder(
-                                        borderRadius: BorderRadius.circular(10.0), // 조절하고 싶은 둥근 정도
-                                      ),
-                                      backgroundColor: isScrapped ? Colors.orange : Colors.white,
-                                      shadowColor: Colors.transparent,
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.end,
+                                  children: [
+                                    ElevatedButton(
+                                        style: ElevatedButton.styleFrom(
+                                            shape: RoundedRectangleBorder(
+                                              borderRadius: BorderRadius.circular(10.0), // 조절하고 싶은 둥근 정도
+                                            ),
+                                          backgroundColor: isLiked ? Colors.blue : Colors.white,
+                                          shadowColor: Colors.transparent,
+                                        ),
+                                        onPressed: (){
+                                          setState(() {
+                                            isLiked = !isLiked;
+                                          });
+                                        },
+                                        child: const Text('공감하기', style: TextStyle(fontSize: 15, color:Colors.black),)
                                     ),
-                                    onPressed: (){
-                                      setState(() {
-                                        isScrapped = !isScrapped; // 버튼 클릭 시 상태 변경
-                                      });
-                                    },
-                                    child: const Text('스크랩', style: TextStyle(fontSize: 15, color:Colors.black),)
+                                    ElevatedButton(
+                                        style: ElevatedButton.styleFrom(
+                                          shape: RoundedRectangleBorder(
+                                            borderRadius: BorderRadius.circular(10.0), // 조절하고 싶은 둥근 정도
+                                          ),
+                                          backgroundColor: isScrapped ? Colors.orange : Colors.white,
+                                          shadowColor: Colors.transparent,
+                                        ),
+                                        onPressed: (){
+                                          setState(() {
+                                            isScrapped = !isScrapped; // 버튼 클릭 시 상태 변경
+                                          });
+                                        },
+                                        child: const Text('스크랩', style: TextStyle(fontSize: 15, color:Colors.black),)
+                                    ),
+                                  ],
                                 ),
                               ],
                             ),
@@ -236,18 +306,6 @@ List<Comment> comments = [
   // 여기에 필요한 만큼 댓글을 추가할 수 있습니다.
 ];
 
-// List<Widget> getContent(){
-//   List<Widget> tiles = [];
-//   comments.forEach((comment) {
-//     tiles.add(ListTile(
-//
-//         title : Text(comment.username),
-//
-//         ));
-//     });
-//   return tiles;
-// }
-
 List<Widget> getContent(){
   List<Widget> tiles = [];
   comments.forEach((comment) {
@@ -257,16 +315,31 @@ List<Widget> getContent(){
           padding: const EdgeInsets.all(8.0),
           child: CircleAvatar(),
         ),
-        Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            Text('nickname', style: TextStyle(fontSize: 10),),
-            Text('comment', style: TextStyle(fontSize: 15),),
-          ],
+        Expanded(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Text('nickname', style: TextStyle(fontSize: 10),),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Text('comment', style: TextStyle(fontSize: 15),),
+                ],
+              ),
+            ],
+          ),
         ),
+        TextButton(onPressed: (){}, child: Text('삭제')),
       ],
 
     ));
   });
   return tiles;
 }
+
+
