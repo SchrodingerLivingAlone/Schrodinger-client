9b5773471c51ebf9b93b280356b61a423b06a41f
dogmania <hj296914@gmail.com>
2023-12-04 23:11:21 +0900

    :sparkles: Feat: 회원가입 api 연결

--- lib/login/join_page.dart
+++ lib/login/join_page.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'dart:io';
 import 'package:image_picker/image_picker.dart';
 import 'package:schrodinger_client/login/google_map_section.dart';
+import 'package:http/http.dart' as http;
+import 'dart:async';
+import 'dart:convert';
+import 'package:http_parser/http_parser.dart';
+
+File? profileImageFile;
 
 class JoinPage extends StatefulWidget {
   const JoinPage({super.key});
@@ -12,6 +19,13 @@ class JoinPage extends StatefulWidget {
 
 class _JoinPageState extends State<JoinPage> {
   final _formKey = GlobalKey<FormState>();
+  String email = '';
+  String password = '';
+  String nickname = '';
+  String city = '';
+  String gu = '';
+  String dong = '';
+  ProfileButton profileButton = const ProfileButton();
 
   @override
   Widget build(BuildContext context) {
@@ -39,6 +53,35 @@ class _JoinPageState extends State<JoinPage> {
                   setState(() {
                     if (_formKey.currentState!.validate()) {
                       _formKey.currentState!.save();
+                      print('${email}');
+                      print('${password}');
+                      print('${nickname}');
+                      print('city: ${city}');
+                      print('gu: ${gu}');
+                      print('dong: ${dong}');
+
+                      if (profileImageFile == null) {
+                        showDialog(
+                          context: context,
+                          builder: (BuildContext context) {
+                            return AlertDialog(
+                              title: const Text('프로필 사진 설정'),
+                              content: const Text('프로필 사진을 설정해주세요.'),
+                              actions: [
+                                TextButton(
+                                  onPressed: () {
+                                    Navigator.pop(context);
+                                  },
+                                  child: const Text('확인'),
+                                ),
+                              ],
+                            );
+                          },
+                        );
+                      } else {
+                        print('${profileImageFile}');
+                        join(context, profileImageFile!!);
+                      }
                     }
                   });
                 },
@@ -59,11 +102,39 @@ class _JoinPageState extends State<JoinPage> {
                   const SizedBox(
                     height: 20,
                   ),
-                  const Center(
-                    child: ProfileButton(),
+                  Center(
+                    child: profileButton,
                   ),
                   const SizedBox(
                     height: 20,
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      TextFormField(
+                        controller: textController,
+                        onTap: () {
+                          Navigator.pushReplacementNamed(context, '/town/auth');  //스택에 넣지 않고 화면 전환만
+                        },
+                        decoration: InputDecoration(
+                            hintText: townAddress == null ? '우리 동네' : '${townAddress.city} ${townAddress.gu} ${townAddress.dong}',
+                            suffixIcon: const Icon(Icons.place),
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10.0),
+                            ),
+                            contentPadding: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 10.0)),
+                        onSaved: (value) {
+                          if (townAddress != null) {
+                            city = townAddress.city;
+                            gu = townAddress.gu;
+                            dong = townAddress.dong;
+                          }
+                        },
+                      )
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 10,
                   ),
                   Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
@@ -82,7 +153,7 @@ class _JoinPageState extends State<JoinPage> {
                           return null;
                         },
                         onSaved: (value) {
-
+                          email = value!.trim();
                         },
                       ),
                       Container(
@@ -113,6 +184,9 @@ class _JoinPageState extends State<JoinPage> {
                           }
                           return null;
                         },
+                        onSaved: (value) {
+                          password = value!.trim();
+                        },
                       ),
                       Container(
                         margin: const EdgeInsets.only(top: 5),
@@ -142,6 +216,9 @@ class _JoinPageState extends State<JoinPage> {
                           }
                           return null;
                         },
+                        onSaved: (value) {
+                          nickname = value!.trim();
+                        },
                       ),
                       Container(
                         margin: const EdgeInsets.only(top: 5),
@@ -152,33 +229,59 @@ class _JoinPageState extends State<JoinPage> {
                       )
                     ],
                   ),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      TextFormField(
-                        controller: textController,
-                        onTap: () {
-                          Navigator.pushNamed(context, '/town/auth');
-                        },
-                        decoration: InputDecoration(
-                          hintText: townAddress == null ? '우리 동네' : '${townAddress.city} ${townAddress.gu} ${townAddress.dong}',
-                          suffixIcon: const Icon(Icons.place),
-                            border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(10.0),
-                            ),
-                            contentPadding: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 10.0)),
-                      )
-                    ],
-                  )
                 ],
               ),
             )
           ),
         )
     );
+  }
+
+  Future<JoinResponse> join(BuildContext context, File profileImage) async {
+    var url = Uri.parse('http://13.124.153.160:8081/api/users/sign-up');
+
+    var request = http.MultipartRequest('POST', url);
+    request.headers['Content-Type'] = 'multipart/form-data';
+
+    request.fields['email'] = email;
+    request.fields['password'] = password;
+    request.fields['nickname'] = nickname;
+    request.fields['city'] = city;
+    request.fields['gu'] = gu;
+    request.fields['dong'] = dong;
+
+    var imageStream = http.ByteStream(Stream.castFrom(profileImage.openRead()));
+    var length = await profileImage.length();
+    var multipartFile = http.MultipartFile('file', imageStream, length, filename: 'file', contentType: MediaType('image', 'jpeg'));
+    request.files.add(multipartFile);
+
+    try {
+      var response = await request.send();
+      var responseBody = await response.stream.bytesToString();
+
+      Map<String, dynamic> jsonMap = json.decode(responseBody);
+
+      // 맵에서 값 추출
+      var isSuccess = jsonMap["isSuccess"];
+      var code = jsonMap["code"];
+      var message = jsonMap["message"];
+      var result = jsonMap["result"];
+
+      if (response.statusCode == 200) {
+        // 성공적으로 응답 받은 경우의 처리
+        print('회원가입 성공');
+        print(responseBody);
+        return JoinResponse.fromJson(json.decode(await response.stream.bytesToString()));
+      } else {
+        // 서버 응답이 실패인 경우의 처리
+        print('회원가입 실패: ${response.statusCode}');
+        print('에러 메시지: $responseBody');
+        throw Exception('Failed to join: ${response.reasonPhrase}');
+      }
+    } catch (e) {
+      // 에러 발생 시의 처리
+      throw Exception('Failed to join: $e');
+    }
   }
 }
 
@@ -200,6 +303,7 @@ class _ProfileButtonState extends State<ProfileButton> {
           if (imageFile != null) {
             setState(() {
               _storedImage = imageFile;
+              profileImageFile = imageFile;
             });
           }
         });
@@ -239,5 +343,80 @@ class _ProfileButtonState extends State<ProfileButton> {
       maxWidth: 600,
     );
     return imageFile != null ? File(imageFile.path) : null;
+  }
+
+  File? getStoredImage() {
+    print('여기까지는 오케이${_storedImage}');
+
+    return _storedImage;
+  }
+}
+
+class JoinResponse {
+  final bool isSuccess;
+  final String code;
+  final String message;
+  final String? result;
+
+  JoinResponse({
+    required this.isSuccess,
+    required this.code,
+    required this.message,
+    required this.result
+  });
+
+  factory JoinResponse.fromJson(Map<String, dynamic> json) {
+    return JoinResponse(
+        isSuccess: json["isSuccess"],
+        code: json["code"],
+        message: json["message"],
+        result: (json['result'])
+    );
+  }
+}
+
+class SignUp{
+  final String email;
+  final String password;
+  final String nickname;
+  final String city;
+  final String gu;
+  final String dong;
+  final dynamic multipartFiles;
+
+  SignUp({
+    required this.email,
+    required this.password,
+    required this.nickname,
+    required this.city,
+    required this.gu,
+    required this.dong,
+    required this.multipartFiles
+  });
+
+  Map<String, dynamic> toJson() {
+    return {
+      'email': email,
+      'password': password,
+      'nickname': nickname,
+      'city': city,
+      'gu': gu,
+      'dong': dong,
+      'multipartFiles': multipartFiles
+    };
+  }
+}
+
+class SignUpRequest {
+  final SignUp signup;
+  final File file;
+
+  SignUpRequest({required this.signup, required this.file});
+
+  Map<String, dynamic> toJson() {
+    return {
+      'signup': signup.toJson(),
+      'file': file,
+    };
   }
 }
